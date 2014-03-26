2becc9e7ca503e58a062b98e2b2e4cc54cba84af
muenier <muenier.elliott@gmail.com>
2014-03-26 15:38:59 +0100

    gege

---
+++ Othello/src/othello/ui/CaseListener.java
@@ -0,0 +1,29 @@
+package othello.ui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import othello.jeu.Jeu;
+
+public class CaseListener implements ActionListener {
+	
+	private Case c;
+	private Jeu jeu;
+	
+	public CaseListener(Case c, Jeu jeu) {
+	   this.c=c;
+	   this.jeu = jeu;
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+	   if (c.isJouable()) {
+	      c.setCouleur(Jeu.tour);
+	      c.getPlateau().returnAllPions(c);
+	      c.getPlateau().setAllCasesNonJouable();
+	      Jeu.changeTour();
+	      jeu.jeuIA();
+	      jeu.setCasesJouables();
+	   }
+	}
+
+}
